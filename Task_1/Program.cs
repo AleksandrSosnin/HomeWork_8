ec87a80374165b8f01575da75dead4b55bb74a87
Aleksandr <sosnin93@yandex.ru>
2023-09-11 13:29:33 +0300

    Task1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -10,3 +10,55 @@
 8 4 4 2
 */
 
+Random random = new Random();
+int rows = 3;
+int cols = 4;
+
+int[,] array = new int[rows, cols];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        array[i, j] = random.Next(1, 100);
+    }
+}
+Console.WriteLine("Исходный массив:");
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 1; j < cols; j++)
+    {
+        int current = array[i, j];
+        int k = j - 1;
+
+        while (k >= 0 && array[i, k] < current)
+        {
+            array[i, k + 1] = array[i, k];
+            k--;
+        }
+
+        array[i, k + 1] = current;
+    }
+}
+Console.WriteLine("Отсортированный массив:");
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
